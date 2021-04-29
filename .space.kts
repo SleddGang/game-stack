7f31260c2049d5a16efe38d0d34da0bae5c57220
Benjamin Brown <benjazzy64@gmail.com>
2021-04-29 08:04:44 +0000

    Switched to using scp

--- .space.kts
+++ .space.kts
@@ -11,11 +11,8 @@ job("Upload docs") {
             interpreter = "/bin/bash"
             content = """
                 echo Uploading swagger.yaml
-                ssh-add - <<< ${'$'}KEY
-                sftp ec2-user@ec2-3-212-113-177.compute-1.amazonaws.com:/var/www/docs.yourgamestack.com/html/rest <<EOF
-				put ./docs/swagger.yaml
-				exit
-				EOF
+                echo "${'$'}KEY" > key.pem
+                scp -i ./key.pem ./docs/swagger.yaml ec2-user@ec2-3-212-113-177.compute-1.amazonaws.com:/var/www/docs.yourgamestack.com/html/rest
             """
         }
     }

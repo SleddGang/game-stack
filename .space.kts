5f427540fe923690524e0dd6e550907ffaaef7af
Benjamin Brown <benjazzy64@gmail.com>
2021-04-29 08:29:55 +0000

    Changed format of key secret

--- .space.kts
+++ .space.kts
@@ -14,7 +14,7 @@ job("Upload docs") {
                 apt-get update
                 apt-get install -y openssh-client
                 echo Uploading swagger.yaml
-                echo "${'$'}KEY" > key.pem
+                echo "${'$'}KEY" | tr "_" "\n" > key.pem
                 scp -i ./key.pem ./docs/swagger.yaml ec2-user@ec2-3-212-113-177.compute-1.amazonaws.com:/var/www/docs.yourgamestack.com/html/rest
             """
         }

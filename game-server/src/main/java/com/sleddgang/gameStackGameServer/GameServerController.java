3cfe40354d52fdbdcbd0bc159deddbeab7ed49d8
benjazzy <benjazzy64@gmail.com>
2021-05-08 20:52:11 -0400

    Added example stomp websocket with spring example.

---
+++ game-server/src/main/java/com/sleddgang/gameStackGameServer/GameServerController.java
@@ -0,0 +1,15 @@
+package com.sleddgang.gameStackGameServer;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class GameServerController {
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public String greeting(String message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return message;
+    }
+}
